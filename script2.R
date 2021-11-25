1215f1a0b1b161afb82573570e115520d8d78759
Miriam Nieves <mirnieves374@gmail.com>
2021-11-25 11:29:06 -0600

    pendiente de revisar colores en script2.R

---
+++ script2.R
@@ -0,0 +1,12 @@
+#programa GIT
+#version 1
+#25 nov 21
+print ("Hola mundo")
+dotchart(swiss$Agriculture,
+         color = rainbow(50),
+         cex = 0.6,
+         labels = row.names(swiss))
+
+#se va a crear una grafica de puntos
+#grafica llamada agricultura con colores de
+#arcoiris y que tenga una tamaño 0.6
